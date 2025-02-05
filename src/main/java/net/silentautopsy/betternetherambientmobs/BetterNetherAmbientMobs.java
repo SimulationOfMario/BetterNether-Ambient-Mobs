08765e697b0a3607d00f27f8c166215818d0319e
Mario Cortés Herrera <imthedarkestspawn@gmail.com>
2025-02-05 13:50:11 +0100

    Init

---
+++ src/main/java/net/silentautopsy/betternetherambientmobs/BetterNetherAmbientMobs.java
@@ -0,0 +1,18 @@
+package net.silentautopsy.betternetherambientmobs;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterNetherAmbientMobs implements ModInitializer
+{
+	public static final String MOD_ID = "betternether-ambientmobs";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
